329e2d53a82602a6a0cd3bd8693c759c1b25147a
Matt Andreko <mandreko@gmail.com>
2012-05-21 11:07:41 -0400

    Added iMessage support.

--- main.rb
+++ main.rb
@@ -5,22 +5,31 @@ require 'sqlite3'
 @local_dump_store = "C:\\dump\\"
 
 def strip_phone_number(number)
-  return number.gsub(/[^a-zA-Z0-9]/, '')
+  number.gsub(/[^a-zA-Z0-9]/, '')
 end
 
 def format_epoch_date(date)
   time = Time.at(date)
-  return time.to_s
+  time.to_s
+end
+
+def format_imessage_date(date)
+  # For some reason, Apple decided that it'd be a good idea to start time at 2001-01-01 00:00:00
+  time = Time.at(date) + 978_307_200 # Seconds difference between epoch and apple's epoch
+  time.to_s
 end
 
 def get_recipient_numbers(db)
   recipients = []
   db.execute("select distinct address from message") do |row|
-    # TODO: figure out why the first address is nil: madrid_handle is iMessage username
     recipients << row['address'] if !row['address'].nil?
   end
 
-  return recipients
+  db.execute("select distinct madrid_handle from message") do |row|
+    recipients << row['madrid_handle'] if !row['madrid_handle'].nil?
+  end
+
+  recipients
 end
 
 def get_backup_folders
@@ -30,17 +39,22 @@ def get_backup_folders
 
     backups << f
   end
-  return backups
+  backups
 end
 
 def get_sms_messages_for_number(db, number)
   messages = []
 
-  db.execute("select date, text from message where address = ? order by date asc", number) do |row|
+  db.execute("select date, text from message where is_madrid = 0 and (address = ? or madrid_handle = ?)", number, number) do |row|
     messages << {:date => format_epoch_date(row['date']), :text => row['text']}
   end
 
-  return messages
+  db.execute("select date, text from message where is_madrid = 1 and (address = ? or madrid_handle = ?)", number, number) do |row|
+    messages << {:date => format_imessage_date(row['date']), :text => row['text']}
+  end
+
+  # Re-sort them, since they may have a mix of sms + iMessage results
+  messages.sort_by! {|obj| obj[:date]}
 end
 
 def dump_sms_messages(file_name)
@@ -52,11 +66,11 @@ def dump_sms_messages(file_name)
     # For each recipient, make an output file to dump the messages to
     Dir.mkdir(@local_dump_store) if !Dir.exists?(@local_dump_store)
 
-    puts "Starting to process #{number}"
     messages = get_sms_messages_for_number(db, number)
 
     # TODO: figure out how to tell who is the sender, and who is the recipient
-    File.open("#{@local_dump_store}#{strip_phone_number(number)}", 'w') {|f|
+    output_file_name = @local_dump_store + strip_phone_number(number)
+    File.open(output_file_name, 'w') {|f|
       messages.each do |msg|
         f.write("#{msg[:date]}: #{msg[:text]}\n")
       end
@@ -67,11 +81,9 @@ end
 
 # Iterate over all backup SMS files
 get_backup_folders().each do |i|
-  file_name = "#{@backup_path}#{i}\\#{@sms_filename}"
+  file_name = @backup_path + i + "\\" + @sms_filename
   next if !File.exists?(file_name)
 
   dump_sms_messages(file_name)
-
-
 end
 
